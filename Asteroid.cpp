0ff54583a6ca0a3b4ebfa1f1385c25793b8b7526
SStalker <raphael@bluepanel.org>
2016-09-01 13:45:20 +0200

    Asteroids move towards the earth with rotation

--- Asteroid.cpp
+++ Asteroid.cpp
@@ -16,3 +16,21 @@ Asteroid::Asteroid(const Vector& startPos, const Vector& startRot)
 {
   loadRessources("assets/model/asteroid.obj", "assets/shader/PhongVertexShader.glsl", "assets/shader/PhongFragmentShader.glsl");
 }
+
+void Asteroid::update(float deltaTime)
+{
+
+  // Set position
+  m_position.translation(pos.X, pos.Y, pos.Z);
+
+  // Set rotation
+  m_rotation.rotationYawPitchRoll(rot.X, rot.Y, rot.Z);
+
+  // Change the position in direction to earth
+  pos = pos - pos * 0.01 * ASTEROID_SPEED;
+
+
+  rot.X -= 0.001;
+  rot.Y -= 0.002;
+  rot.Z -= 0.004;
+}
